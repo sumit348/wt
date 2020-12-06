9179f8b9fdab90de2a6310c6f3312cba60552c08
sumit348 <73032984+sumit348@users.noreply.github.com>
2020-12-07 00:53:47 +0600

    Add files via upload

---
+++ final/Final labtask-5/l3.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>CSS Grid Responsive Site</title>
+</head>
+
+<body>
+    <!-- Header Showcase -->
+    <header id="showcase" class="grid">
+        <div class="bg-image"></div>
+        <div class="content-wrap">
+            <h1>Welcome to Doctors Lab</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci eum error earum soluta voluptatum
+                nisi laboriosam eos saepe asperiores dolorum.</p>
+            <a class="btn">Read More</a>
+        </div>
+    </header>
+
+    <!-- Main Area -->
+    <main id="main">
+        <!-- Section A -->
+        <section id="section-a" class="grid">
+            <div class="content-wrap">
+                <h2 class="content-title">About Doctors Lab</h2>
+                <div class="content-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe sint eligendi possimus? Unde
+                        officiis magnam laborum ipsa distinctio odio, vero dolores dicta aliquam aperiam repellendus.
+                        Perferendis officiis deserunt velit voluptas nobis sequi
+                        animi totam, accusantium, ex eius quia, natus quo?</p>
+                </div>
+            </div>
+        </section>
+
+        <!-- Section B -->
+        <section id="section-b" class="grid">
+            <ul>
+                <li>
+                    <div class="card">
+                        <img src="1.png" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Primary Care</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <img src="2.jpg" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Online Appointment</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="card">
+                        <img src="5.jpg" alt="">
+                        <div class="card-content">
+                            <h3 class="card-title">Emergency Cases</h3>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum culpa neque quo eum et
+                                quasi velit voluptatum cum maiores exercitationem.</p>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </section>
+
+        <!-- Section C -->
+        <section id="section-c" class="grid">
+            <div class="content-wrap">
+                <h2 class="content-title">We Handle Your Health Support</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime nam rerum vel earum error fugiat
+                    cupiditate, dolore eius! Minus, explicabo.</p>
+            </div>
+        </section>
+
+        <!-- Section D -->
+        <section id="section-d" class="grid">
+            <div class="box">
+                <h2 class="content-title">Contact Us</h2>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum, suscipit. Rerum ducimus a quod, ut et
+                    voluptas obcaecati unde fuga.</p>
+                <p>contact@sumit.mh07@gmail.com</p>
+            </div>
+            <div class="box">
+                <h2 class="content-title">About Our Policy</h2>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio earum porro deserunt, deleniti,
+                    quae facere repudiandae, officiis est exercitationem nobis iusto doloremque! Soluta excepturi in
+                    aut suscipit amet temporibus quo?</p>
+            </div>
+        </section>
+    </main>
+
+    <!-- Footer -->
+    <footer id="main-footer" class="grid">
+        <div>Doctors Lab</div>
+        <div>Project By <a href="#" target="_blank">Mehedi Hasan</a></div>
+    </footer>
+
+</body>
+
+</html>
